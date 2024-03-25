36db9c5a987d6ca3ec0502ebe94ef0818618393c
shivansh <shivneeraj2004@gmail.com>
2024-03-25 23:25:32 +0530

    zod

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,21 @@ import { z } from "zod";
 // type User = {
 //   username: string;
 // };
-
+const hobbies = ["Programming", "weight Lifting", "Guitar"] as const;
 const UserSchema = z.object({
   username: z.string().min(3), //min length 3
-  age: z.number().gt(0).optional(), //gt than zero
+  age: z.number().gt(0).default(Math.random()), //gt than zero
   birthday: z.date().optional(),
   isProgrammer: z.boolean().default(true).optional(), // can be null
   test: z.any(),
+  hello: z.literal(true),
+  hobby: z.enum(hobbies),
 });
 
 type User = z.infer<typeof UserSchema>;
 
 const user: User = {
   username: "shivansh",
-  age: 20,
-  birthday: new Date(),
 };
 
-console.log(UserSchema.parse(user));
+console.log(UserSchema.partial().parse(user));
